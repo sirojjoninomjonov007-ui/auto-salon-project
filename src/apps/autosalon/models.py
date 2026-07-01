2d364f0339ba3a8e93a1b8826fe1bbfc0a514803
sirojjon <sirojjoninomjonov007@gmail.com>
2026-07-01 12:43:40 +0500

    models.py

---
+++ src/apps/autosalon/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+
+# Create your models here.
+
+
+class User(models.Model):
+    name = models.CharField
+    last_name = models.CharField(blank=True, null=True)
+    age = models.PositiveIntegerField(default=0)
+    phone_number = models.CharField(blank=True, null=True)
+    birthday = models.DateField(blank=True, null=True)
+
+
+
+class Car(models.Model):
+    name = models.CharField(max_length=50)
+    brand = models.CharField(max_length=50)
+    year = models.PositiveIntegerField(default=2020)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    colour = models.CharField
+    engine_type = models.CharField
+    status = models.CharField(default='available')
+
+
+class Order(models.Model):
+    user = models.ForeignKey(models.CASCADE)
+    car = models.ForeignKey()
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+
+class TestDrive(models.Model):
+    user = models.ForeignKey
+    car = models.ForeignKey( models.CASCADE)
+    status = models.CharField()
+
+
+
+
+
