41705d1cb08b4afd95694f9cafafde705a965572
sirojjon <sirojjoninomjonov007@gmail.com>
2026-07-06 20:48:58 +0500

    new

--- src/apps/autosalon/models.py
+++ src/apps/autosalon/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class User(models.Model):
-    name = models.CharField(max_length=50) # max_length qo'shildi
-    last_name = models.CharField(max_length=50, blank=True, null=True) # max_length qo'shildi
+    name = models.CharField(max_length=50) 
+    last_name = models.CharField(max_length=50, blank=True, null=True) 
     age = models.PositiveIntegerField(default=0)
-    phone_number = models.CharField(max_length=20, blank=True, null=True) # max_length qo'shildi
+    phone_number = models.CharField(max_length=20, blank=True, null=True) 
     birthday = models.DateField(blank=True, null=True)
 
     def __str__(self):
@@ -16,16 +16,15 @@ class Car(models.Model):
     brand = models.CharField(max_length=50)
     year = models.PositiveIntegerField(default=2020)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    colour = models.CharField(max_length=30) # max_length qo'shildi
-    engine_type = models.CharField(max_length=30) # max_length qo'shildi
-    status = models.CharField(max_length=30, default='available') # max_length qo'shildi
+    colour = models.CharField(max_length=30) 
+    engine_type = models.CharField(max_length=30) 
+    status = models.CharField(max_length=30, default='available') 
 
     def __str__(self):
         return f'{self.brand} {self.name}'
 
 
 class Order(models.Model):
-    # To'g'ri ForeignKey bog'lanishi
     user = models.ForeignKey(User, on_delete=models.CASCADE) 
     car = models.ForeignKey(Car, on_delete=models.CASCADE) 
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
@@ -36,11 +35,9 @@ class Order(models.Model):
 
 
 class TestDrive(models.Model):
-    # To'g'ri ForeignKey bog'lanishi
     user = models.ForeignKey(User, on_delete=models.CASCADE) 
     car = models.ForeignKey(Car, on_delete=models.CASCADE) 
-    # Bitta status maydoni qoldirildi (biri matn, biri raqam edi, raqamlisini qoldirdik)
-    status = models.PositiveSmallIntegerField(default=0) 
+    status = models.PositiveIntegerField(default=0) 
 
     def __str__(self):
         return f"TestDrive {self.user.name} - {self.car.brand}"
